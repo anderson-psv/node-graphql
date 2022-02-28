2bf6b9a5254f1a4535e0214343aa4edabcccc804
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-02-27 21:51:04 -0300

    -Added 'apollo-server' working with docker container.

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,1 +1,21 @@
-console.log('Test');
+const {ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    #Points os entrance of API
+    type Query {
+        ola: String
+    }
+`
+
+const resolvers = {
+
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(({ url }) => {
+    console.log(`'Executando em ${url}`)
+})
