19cb67153069d7f3db6164513bc9c9d00e2121ac
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-02-27 22:46:46 -0300

    Added type User with resolver in salary.

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -3,21 +3,45 @@ const {ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
     scalar Date
 
+    type User {
+        id: ID
+        name: String!
+        email: String!
+        age: Int
+        real_salary: Float
+        vip: Boolean
+    }
+
     #Points of entrance from API
     type Query {
-        hello: String
+        hello: String!
         timeNow: Date!
+        logedUser: User
     }
 `
 
-//process.env.TZ = 'America/Sao_Paulo'
 const resolvers = {
+    User: {
+        salary(user) {
+            return user.real_salary
+        }
+    },
     Query: {
         hello() {
             return "Hello and welcome to my first graphql node query! :)";
         },
         timeNow() {
             return new Date;
+        },
+        logedUser() {
+            return {
+                'id': '321',
+                'name': 'Anderson PS',
+                'email': 'anderson_test@email.com',
+                'age': 29,
+                'real_salary': 1278.89,
+                'vip': true
+            };
         }
     }
 }
