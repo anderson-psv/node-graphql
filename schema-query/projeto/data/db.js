4af1cf6785bb2d0365f4a9fefc71b5a4909438ac
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-03-02 23:29:24 -0300

    Added example with UserStatus using Enum.

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -4,21 +4,24 @@ const users = [
         name: 'Felipe Craum',
         email: 'fcraum@email.com',
         age: 35,
-        profile_id: '0002'
+        profile_id: '0002',
+        status: 'BLOCKED'
     },
     {
         id: '2',
         name: 'Fernando Abraum',
         email: 'fabraum@email.com',
         age: 21,
-        profile_id: '0001' 
+        profile_id: '0001',
+        status: 'INACTIVE' 
     },
     {
         id: '3',
         name: 'Maila Fredo',
         email: 'mfredo@email.com',
         age: 26,
-        profile_id: '0002'
+        profile_id: '0002',
+        status: 'ACTIVE'
     }
 ];
 
@@ -33,4 +36,4 @@ const profiles = [
     }
 ];
 
-module.exports = { users, profiles }
+module.exports = { users, profiles };
