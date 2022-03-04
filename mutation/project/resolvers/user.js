1798c564f311d704e4a12caf8d50199d87b7fe28
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-03-03 22:43:05 -0300

    Fix variable name in portuguese

--- mutation/project/resolvers/user.js
+++ mutation/project/resolvers/user.js
@@ -3,7 +3,7 @@ const { profiles } = require('../data/db')
 module.exports = {
     profile(user) {
         const selecteds = profiles
-            .filter(p => p.id === usuario.profile_id)
+            .filter(p => p.id === user.profile_id)
         return selecteds ? selecteds[0] : null
     }
 }
