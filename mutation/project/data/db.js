fa21fa1f9d3ac53fd4fc89ee48dbdc31db14e030
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-03-03 22:24:25 -0300

    Added function nextId for users.

--- mutation/project/data/db.js
+++ mutation/project/data/db.js
@@ -1,24 +1,30 @@
+let id = 1;
+
+function nextId() {
+    return id++
+}
+
 const profiles = [
     { id: '0001', name: 'administrator' },
     { id: '0002', name: 'common' }
 ]
 
 const users = [{
-    id: 1,
+    id: nextId(),
     name: 'João Silva',
     email: 'jsilva@zemail.com',
     age: 29,
     profile_id: '0001',
     status: 'ACTIVE'
 }, {
-    id: 2,
+    id: nextId(),
     name: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     age: 31,
     profile_id: '0002',
     status: 'INACTIVE'
 }, {
-    id: 3,
+    id: nextId(),
     name: 'Daniela Smith',
     email: 'danismi@umail.com',
     age: 24,
