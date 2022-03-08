01786c22d510d56e2a8763f704bb8a21dc7cda89
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-03-07 23:17:56 -0300

    Added first example of input in graphql.

--- mutation/project/resolvers/mutation.js
+++ mutation/project/resolvers/mutation.js
@@ -1,9 +1,9 @@
 const { users, nextId } = require("../data/db");
 
 module.exports = {
-    newUser(_, args) {
+    newUser(_, data) {
         //Check if the email has already been registered
-        const existingEmail = users.some((u) => u.email === args.email);
+        const existingEmail = users.some((u) => u.email === data.email);
 
         if (existingEmail) {
             throw new Error("E-mail already exists!");
@@ -11,7 +11,7 @@ module.exports = {
 
         const new_user = {
             id: nextId,
-            ...args, //Add all args as keys and values
+            ...data, //Add all data as keys and values
             profile_id: "0001",
             status: "ACTIVE",
         };
