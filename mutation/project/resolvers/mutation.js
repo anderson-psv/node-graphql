cd8636004c18e1bffa5b1f2a457b7531f4e7f43a
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-03-07 22:52:52 -0300

    Added example to delete user

--- mutation/project/resolvers/mutation.js
+++ mutation/project/resolvers/mutation.js
@@ -1,22 +1,30 @@
 const { users, nextId } = require("../data/db");
 
 module.exports = {
-  newUser(_, args) {
-    //Check if the email has already been registered
-    const existingEmail = users.some(u => u.email === args.email);
+    newUser(_, args) {
+        //Check if the email has already been registered
+        const existingEmail = users.some((u) => u.email === args.email);
 
-    if (existingEmail) {
-        throw new Error("E-mail already exists!")
-    }
+        if (existingEmail) {
+            throw new Error("E-mail already exists!");
+        }
 
-    const new_user = {
-      id: nextId,
-      ...args, //Add all args as keys and values
-      profile_id: "0001",
-      status: "ACTIVE",
-    };
+        const new_user = {
+            id: nextId,
+            ...args, //Add all args as keys and values
+            profile_id: "0001",
+            status: "ACTIVE",
+        };
 
-    users.push(new_user);
-    return new_user;
-  },
+        users.push(new_user);
+        return new_user;
+    },
+    deleteUser(_, { id }) {
+        const index = users.findIndex((u) => u.id === id);
+
+        if (index < 0) return null;
+        const deleteds = users.splice(index, 1);
+
+        return deleteds ? deleteds[0] : null;
+    },
 };
