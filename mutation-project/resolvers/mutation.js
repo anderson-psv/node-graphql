6e7c1e93f2062e0ca4c7579565af8e95a39470bd
Anderson PSV <andersonpintodasilva@hotmail.com>
2022-03-10 22:16:36 -0300

    Added filter with input grahql on delete

--- mutation-project/resolvers/mutation.js
+++ mutation-project/resolvers/mutation.js
@@ -1,9 +1,21 @@
 const { users, nextId } = require("../data/db");
 
+function userIndex(filter) {
+    if (!filter) return -1;
+    const { id, email } = filter;
+
+    if (id) {
+        return users.findIndex(u => u.id === id);
+    } else if (email) {
+        return users.findIndex(u => u.email === email);
+    }
+    return -1;
+}
+
 module.exports = {
     newUser(_, data) {
         //Check if the email has already been registered
-        const existingEmail = users.some((u) => u.email === data.email);
+        const existingEmail = users.some(u => u.email === data.email);
 
         if (existingEmail) {
             throw new Error("E-mail already exists!");
@@ -19,8 +31,8 @@ module.exports = {
         users.push(new_user);
         return new_user;
     },
-    deleteUser(_, { id }) {
-        const index = users.findIndex((u) => u.id === id);
+    deleteUser(_, { filter }) {
+        const index = userIndex(filter);
 
         //User not found
         if (index < 0) return null;
@@ -30,7 +42,7 @@ module.exports = {
         return deleteds ? deleteds[0] : null;
     },
     updateUser(_, args) {
-        const index = users.findIndex((u) => u.id === args.id);
+        const index = users.findIndex(u => u.id === args.id);
 
         //User not found
         if (index < 0) return null;
